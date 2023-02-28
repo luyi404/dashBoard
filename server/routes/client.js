d89c48a4951417e347dafa5abf5827d608ae4e74
luyi404 <370977893@qq.com>
2023-02-28 18:58:30 -0500

    [ftr] Implement Gerography api

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import {getProduct, getCustomers, getTransactions} from "../controllers/client.js"
+import {getProduct, getCustomers, getTransactions, getGeography} from "../controllers/client.js"
 
 const router = express.Router();
 router.get("/products", getProduct);
 router.get("/customers", getCustomers);
 router.get("/transactions", getTransactions);
+router.get("/geography", getGeography);
 
 export default router;
