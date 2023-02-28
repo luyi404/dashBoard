cf9cca538c5a48f4eadcae9013d763064a337d9b
luyi404 <370977893@qq.com>
2023-02-28 15:11:42 -0500

    Add Customers scene and fix some error status codes

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -18,6 +18,15 @@ export const getProduct = async (req, res) => {
         );
         res.status(200).json(productsWithStats);
     } catch (error) {
-        res.status(404).json({message: error.message})
+        res.status(500).json({message: error.message})
     }
 };
+
+export const getCustomers = async (req, res) => {
+    try {
+        const customers = await User.find({role: "user"}).select("-password");
+        res.status(200).json(customers)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
--- server/routes/client.js
+++ server/routes/client.js
@@ -1,8 +1,9 @@
 import express from "express";
 
-import {getProduct} from "../controllers/client.js"
+import {getProduct, getCustomers} from "../controllers/client.js"
 
 const router = express.Router();
-router.get("/products", getProducts);
+router.get("/products", getProduct);
+router.get("/customers", getCustomers)
 
 export default router;
