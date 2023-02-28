91e2474657f3907ead835fe4fda4f5cc6d722f95
luyi404 <370977893@qq.com>
2023-02-28 16:58:06 -0500

    [ftr] adding Transactions api for server side pagination

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-import {getProduct, getCustomers} from "../controllers/client.js"
+import {getProduct, getCustomers, getTransactions} from "../controllers/client.js"
 
 const router = express.Router();
 router.get("/products", getProduct);
-router.get("/customers", getCustomers)
+router.get("/customers", getCustomers);
+router.get("/transactions", getTransactions);
 
 export default router;
