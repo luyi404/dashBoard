c606f4f56673fcda1cb20006c2c02debebec857d
luyi404 <370977893@qq.com>
2023-02-27 21:38:11 -0500

    [ftr] add two api for Product and ProductStat

---
+++ server/models/ProductStat.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const ProductStatSchema = new mongoose.Schema(
+    {
+        ProductId: String,
+        yearlySalesTotal: Number,
+        yearlyTotalSoldUnits: Number,
+        year: Number,
+        monthlyData: [
+            {
+                month: String,
+                totalSales: Number,
+                totalUnits: Number,
+            }
+        ],
+        dailyData: [
+            {
+                day: String,
+                totalSales: Number,
+                totalUnits: Number,
+            }
+        ],
+    }, {timestamps: true}
+);
+
+const ProductStat = mongoose.model("ProductStat", ProductStatSchema);
+export default ProductStat;
